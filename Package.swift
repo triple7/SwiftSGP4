2e9bef3c9a4490a8d02e245aaf6622d5b90f42f4
Yuma Antoine Decaux <yuma@astreos.space>
2023-07-01 13:06:56 +1000

    added SGP4 as library part

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "SwiftSGP4",
-            targets: ["SwiftSGP4"]),
+            targets: ["SGP4", "SwiftSGP4"]),
     ],
     dependencies: [
         .package(url: "https://github.com/triple7/SwiftCelesTrak", branch:"main"),
