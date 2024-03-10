7e3a5ac1dbf8d013bc8786ba86fd3a6480f97480
Yuma Antoine Decaux <yuma@astreos.space>
2024-03-10 18:52:29 +1100

    aded setActive by norad_id in SwiftSpg4+utilities

---
+++ Sources/SwiftSGP4/SwiftSGP4+utils.swift
@@ -0,0 +1,22 @@
+//
+//  File.swift
+//  
+//
+//  Created by Yuma decaux on 10/3/2024.
+//
+
+import Foundation
+import SwiftCelesTrak
+
+extension SwiftSGP4 {
+    
+    
+    public func toggleActive(noradIds: [Int], active: Bool) {
+        DispatchQueue.concurrentPerform(iterations: self.targetCount, execute:  { i in
+            if noradIds.contains(self.targets[i].NORAD_CAT_ID) {
+                self.targets[i].setActive(active: active)
+            }
+        })
+    }
+    
+}
